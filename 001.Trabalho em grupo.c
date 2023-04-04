e51ecc03261d8deb8b56a089ecd0b82bb11c5d22
Renxtt <122226822+Renxtt@users.noreply.github.com>
2023-04-04 09:40:38 -0300

    Add files via upload

---
+++ 001.Trabalho em grupo.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+#include <windows.h>
+#include <math.h>
+#include <time.h>
+
+main(void){
+	
+	setlocale(LC_ALL, "portuguese");
+	unsigned int cp = 1252;
+	unsigned int cpin = GetConsoleCP();
+	unsigned int cpout = GetConsoleOutputCP();
+	SetConsoleCP(cp);
+	SetConsoleOutputCP(cp);
+	
+	
+//fazendo funeraria
+
+
+	//printar informações sobre a funeraria -1
+		
+		//nome da funeraria
+		
+		//desde quando ela esta em funcionamento
+		
+		//horaria de trabalho
+		
+		//telefone da funeraria
+		
+		//localização da funeraria
+		
+	
+	//perguntar informações pessoais
+	
+		//nome da pessoa que recebera a cerimonia
+	
+	
+		//perguntar o nome de quem vai realizar a cerimonia
+	
+		
+		//tipo de cerimonia
+		
+			//formas da cerimonia
+			
+				//enterro
+				
+					//modelos pré prontos de caixão com P M G e personalizado
+	
+													
+				//cremação
+					
+					//informar estilos preprontos de potes para colocar as cinzas(5 tipos no minimo)
+					
+				//se vai ou não adicionar flores
+				
+					//se sim adicionar um preço extra pelas flores
+				
+			
+	//marcar a data da cerimonia e mostrar quanto tempo falta para essa data
+		
+	//adicionar 1000 R$ preço de transporte
+
+	//perguntar se é evento fechado
+		
+		//se for fechado
+		
+			//mostrar preço por pessoa
+		
+			// perguntar quantidade de convidados
+			
+			//perguntar nome dos candidatos
+		
+		//se não for fechado
+			
+			//apenas continuar
+			
+			//adiciona 2000 R$
+			
+	//calcular o valor da cerimonia levando em conta o preço por pessoa
+		
+		//mostrar certificado(informações gerais)
+		
+		
+		
+	
+	
+	return 0;
+}
